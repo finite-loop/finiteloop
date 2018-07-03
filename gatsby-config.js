a7f9181f743e0bc555efca6ddecc33f527467689
partha360 <partha@finiteloop.io>
2018-07-03 15:14:57 +0530

    Add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-netlify',
   ],
 }
