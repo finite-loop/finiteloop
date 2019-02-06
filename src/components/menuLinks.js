9e5827f33840b9fc77def9350c958dc6c9021aec
Parthasarathy S <parthasarathy.s@mindtree.com>
2019-02-06 20:49:14 +0530

    Styling for phones with notch

--- src/components/menuLinks.js
+++ src/components/menuLinks.js
@@ -8,7 +8,7 @@ const MenuLinks = ({ links, social, classes }) => (
     <div className="text-sm lg:flex-grow" />
     {links.map(({ item }) => (
       <Link key={item.title} className="link-primary" to={item.url}>
-        <span style={{ lineHeight: '5vh' }}>{item.title}</span>
+        <span style={{ lineHeight: '4.4vh' }}>{item.title}</span>
       </Link>
     ))}
     {/* <a href={social.lnkdnUrl} target="_new" className="link-primary">
@@ -18,7 +18,7 @@ const MenuLinks = ({ links, social, classes }) => (
       <TwtrIcon style={{ height: '4.5vh' }} alt="Twitter" />
     </a> */}
     <a href={social.githubUrl} target="_new" className="link-primary">
-      <GitHubIcon style={{ height: '4.5vh' }} alt="Github" />
+      <GitHubIcon style={{ height: '4vh' }} alt="Github" />
     </a>
     {/* <a href={social.mediumUrl} target="_new" className="link-primary">
       <MediumIcon style={{ height: '4.5vh' }} alt="Medium" />
