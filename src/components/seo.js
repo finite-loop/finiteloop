487d17712819af3e501d6d4d7e41920b8f7f07b2
Parthasarathy S <parthasarathy.s@mindtree.com>
2019-02-15 14:16:27 +0530

    Update favicon, manifest logos

--- src/components/seo.js
+++ src/components/seo.js
@@ -98,79 +98,25 @@ class SEO extends Component {
       <Helmet>
         <link
           rel="apple-touch-icon"
-          sizes="57x57"
-          href="img/favicons/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="img/favicons/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="img/favicons/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="img/favicons/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="img/favicons/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="img/favicons/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="img/favicons/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="img/favicons/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
           sizes="180x180"
-          href="img/favicons/apple-icon-180x180.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="img/favicons/android-icon-192x192.png"
+          href="img/favicons/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="img/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="img/favicons/favicon-96x96.png"
+          href="img/faviconsfavicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="img/favicons/favicon-16x16.png"
+          href="img/faviconsfavicon-16x16.png"
         />
         <link rel="manifest" href="img/favicons/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="FiniteLoop" />
+        <meta name="application-name" content="FiniteLoop" />
         <meta name="msapplication-TileColor" content="#ba4f1a" />
-        <meta
-          name="msapplication-TileImage"
-          content="img/favicons/ms-icon-144x144.png"
-        />
         <meta name="theme-color" content="#ba4f1a" />
         {/* General tags */}
         <meta name="description" content={description} />
