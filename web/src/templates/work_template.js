e0a9ebd86308f29e1c5d44cca60313bc03cf502d
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-10-21 12:18:47 -0700

    initialized work template

--- web/src/templates/work_template.js
+++ web/src/templates/work_template.js
@@ -1,7 +1,8 @@
 import React from "react"
+import "../pages/index.css"
 
 const work_template = () => {
-  return <div></div>
+  return <div className="text-xl">Hello From Work Page!!</div>
 }
 
 export default work_template
