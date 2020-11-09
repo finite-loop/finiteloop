513e1d8f95878b760968b0b67d29b4fe4f265afd
Adarsh Menon <adarsh1021@gmail.com>
2020-11-09 10:49:53 +0530

    change react form validator to react hook form

--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -2,8 +2,7 @@ import React, {useState, useRef} from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { useStaticQuery, graphql } from "gatsby"
-import { ValidatorForm } from 'react-form-validator-core'
-import TextValidator from '../components/TextValidator'
+import { useForm } from "react-hook-form";
 
 const ContactPage = () => {
     const data = useStaticQuery(graphql`
@@ -24,21 +23,10 @@ const ContactPage = () => {
   `)
 
   const contactData = data.sanityContactUs
-    
-  const formRef = useRef()
-  const [formData, setFormData] = useState({
-    firstname: '',
-    lastname: '',
-    email: '',
-    message: '',
-    company: '',
-  });
 
-  const handleChange = (event) => {
-    setFormData({...onloadeddata, [event.target.name]: event.target.value})
-  }
+  const { handleSubmit, register, errors } = useForm();
 
-  const handleSubmit = (e) => {
+  const onSubmit = (formData) => {
     fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
@@ -51,7 +39,6 @@ const ContactPage = () => {
           console.error('Form submission error:', error);
           alert("Sorry, we have trouble submitting your form. Please try again later");
         })
-    e.preventDefault()
   }
 
   return (
@@ -104,88 +91,76 @@ const ContactPage = () => {
                 {contactData.SubHeading}
               </p>
               <div className="flex flex-col sm:px-2">
-                <ValidatorForm
-                  onSubmit={handleSubmit}
-                  onError={errors => console.log(errors)}
-                  name="Contact"
-                  ref={formRef}
-                  data-netlify="true"
-                  data-netlify-honeypot="bot-field"
-                  className="pb-5"
-                >
-                  <TextValidator
+                <form className="pb-5" onSubmit={handleSubmit(onSubmit)}>
+                  <input
                     id="firstname"
                     name="firstname"
                     placeholder="First Name *"
-                    value={formData.firstname}
-                    onChange={handleChange}
-                    validators={['required']}
-                    errorMessages={['This field is required']}
                     margin="normal"
-                    className="input-field"
+                    className="input-field mb-1"
+                    ref={register({
+                      required: "This field is required"
+                    })}
                   />
-                  
-                  <TextValidator
+                  {errors.firstname && errors.firstname.message}
+                  <input
                     id="lastname"
                     name="lastname"
                     placeholder="Last Name *"
-                    value={formData.lastname}
-                    onChange={handleChange}
-                    validators={['required']}
-                    errorMessages={['This field is required']}
                     margin="normal"
-                    className="input-field"
+                    className="input-field mt-2 mb-1"
+                    ref={register({
+                      required: "This field is required"
+                    })}
                   />
-                  <TextValidator
+                  {errors.lastname && errors.lastname.message}
+                  <input 
                     id="email"
-                    name="email"
+                    name="email" 
                     placeholder="E-mail *"
-                    value={formData.email}
-                    onChange={handleChange}
-                    validators={['required', 'isEmail']}
-                    errorMessages={[
-                      'This field is required',
-                      'E-mail is not valid',
-                    ]}
                     margin="normal"
-                    className="input-field"
-                  />
-                  <TextValidator
+                    className="input-field mt-2 mb-1" 
+                    ref={register({
+                      required: "This field is required",
+                      pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: "E-mail is not valid"
+                      }
+                    })}/>
+                    {errors.email && errors.email.message}
+                  <input
                     id="company"
                     name="company"
                     placeholder="Company Name"
-                    value={formData.company}
-                    onChange={handleChange}
                     margin="normal"
-                    className="input-field"
+                    className="input-field mt-2 mb-1"
+                    ref={register()}
                   />
-                  <TextValidator
+                  <textarea
                     id="message"
                     name="message"
                     placeholder="Message *"
-                    multiLine
                     rows={4}
-                    value={formData.message}
-                    onChange={handleChange}
-                    validators={['required']}
-                    errorMessages={['This field is required']}
                     margin="normal"
-                    className="input-field"
+                    className="input-field mt-2 mb-1"
+                    ref={register({
+                      required: "This field is required"
+                    })}
                   />
-                  <input name="bot-field" style={{ display: 'none' }} />
-                  <br />
+                  {errors.message && errors.message.message}
                   <div className="float-right">
                     <button
                       role="submit"
                       aria-label="Submit"
                       type="submit"
                       className="rectButton"
+                      style={{cursor: "pointer"}}
                     >
                       Submit
                     </button>
                   </div>
-                </ValidatorForm>
-              </div> 
+                </form>
+              </div>
               <SEO
                 postPath={'/contact'}
               />
